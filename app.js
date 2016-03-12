9966ae57a95247b0b89dc331c6bd54c87a6ceada
ebrimatunkara <ebrimatunkara@gmail.com>
2016-03-11 19:34:25 -0500

    update request reponse message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello world jenkinsss - continious integration testing app');
+  res.send('Hello world testing app - jenkins continious integration');
 });
 
 app.listen(process.env.PORT || 5000);
